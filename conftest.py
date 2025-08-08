bb62913ced71de81b335b7ea3c1832d7f5fea584
NayeemJohnY <njnayeem@gmail.com>
2025-08-08 14:57:23 +0530

    Update confest.py nosec

--- conftest.py
+++ conftest.py
@@ -25,4 +25,5 @@ def pytest_sessionfinish(session, exitstatus):
     client = APIClient(BASE_URI, BASE_PATH)
     res = client.delete(client.build_url("/reset"),
                         headers={"authorization": "Bearer admin-token"})
+    # nosec
     assert res.status_code == 204, f"Reset failed: {res.status_code} {res.text}"
